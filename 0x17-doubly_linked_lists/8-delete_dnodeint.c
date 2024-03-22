9c09075a0c0be7d4cded07664bf894892a4709f9
LichtCode <prinzdarey@gmail.com>
2024-03-22 04:00:06 +0100

    made more adjustments

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - the function deletes the node at index of a
+ * dlistint_t linked list
+ *
+ * @head: The head of the list
+ * @index: The index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+ 
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+
+	return (1);
+}
